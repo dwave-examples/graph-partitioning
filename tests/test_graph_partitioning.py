48baf27d40502a4243d83fa8e57a28d1db8945da
Melody Wong <mwong@dwavesys.com>
2020-02-21 17:38:48 -0800

    Grab appropriate python interpreter

--- tests/test_graph_partitioning.py
+++ tests/test_graph_partitioning.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 import unittest
 
 # /path/to/demos/graph-partitioning/tests/test_graph_partitioning.py
@@ -11,4 +12,4 @@ class TestDemo(unittest.TestCase):
         """run graph_partitioning.py and check that nothing crashes"""
 
         demo_file = os.path.join(project_dir, 'graph_partitioning.py')
-        subprocess.check_output(["python", demo_file])
+        subprocess.check_output([sys.executable, demo_file])
